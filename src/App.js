294580b0f8e63d8b1334b884f42b15190778f1fb
venkat ravilla <venkatravilla333@gmail.com>
2025-10-11 09:26:37 +0530

    jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import ClassCom from "./Component/ClassCom"
 import FunCom from "./Component/FunCom"
+import Jsx from "./JSX/Jsx"
 
 
 function App() {
-  var age = 40
+  // var age = 40
   return <div>
-    <FunCom age={age} />
-    <ClassCom/>
+    {/* <FunCom age={age} />
+    <ClassCom/> */}
     {/* {new ClassCom().render()} */}
     {/* {FunCom()} */}
+    <Jsx/>
   </div>
 }
 
---
+++ src/JSX/Jsx.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function Jsx() {
+
+  // var x = 'sachin'
+  // var x = 40
+  // var x = [40]
+  // var x = {value: 40}
+  // var x = function () {
+  //   return 40
+  // }
+  // var x = true
+  // var x = false
+  // var x = null
+  // var x = undefined
+
+  // return <h3>value: </h3>
+  // return (
+  //   <div>
+  //     <h1 id='one'>This is JSX</h1>
+  //   </div>
+  // )
+  //  return React.createElement('div', {}, React.createElement('h1', {id: 'one'}, 'Without using JSX'))
+  // return <h1>Hello</h1>
+  //        <p>Hello</p>
+  // return <div id='hello'>
+  //   <h1>Hello</h1>
+  //   <p>Hello</p>
+  // </div> 
+  // return <>
+  //        <h1>Hello</h1>
+  //        <p>Hello</p>
+  //       </> 
+  // return [ <h1>Hello</h1>,
+  //        <p>Hello</p>]
+        
+        
+}
+
+export default Jsx
